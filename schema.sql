0b7ffd815ca6110e4bb1ee81350aa4900bb4ed79
Brevenn <Brevenjh@gmail.com>
2022-10-31 20:06:58 -0500

    added table for employees and sub text to explain

--- schema.sql
+++ schema.sql
@@ -1,10 +1,22 @@
+-- create database.
 DROP DATABASE IF EXISTS employeeTracker_DB;
 CREATE DATABASE employeeTracker_DB;
 
 USE employeeTracker_DB;
 
+-- create data table for one piece character types.
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
     department_name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
+);
+
+-- create table for character roles.
+CREATE TABLE employee_role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title CARCHAR(30) NOT NULL,
+    berries DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 )
