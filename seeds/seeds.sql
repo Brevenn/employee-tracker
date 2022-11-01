d35fd65027d08fc9f49a514aebc0afe9f90fe879
Brevenn <Brevenjh@gmail.com>
2022-10-31 19:25:13 -0500

    created seed for roles

--- seeds/seeds.sql
+++ seeds/seeds.sql
@@ -1,7 +1,18 @@
 INSERT INTO department (id, department_name)
-VALUES (1, "Pirate Captain"),
-       (2, "Yonkou"),
-       (3, "Navy Admiral"),
-       (4, "Warlord of the Sea"), 
-       (5, "Worst Generation Pirate"), 
-       (6, "Crew Member");
+VALUES (1, "Strong Pirate"),
+       (2, "Emperor of the Sea"),
+       (3, "Navy"),
+       (4, "ShiChibukai"), 
+       (5, "Worst Generation"), 
+       (6, "Civilian");
+
+INSERT INTO employee_role (id, title, berries, department_id)
+VALUES (1, "Captain", 850000000, 1),
+       (2, "Vice-Captain", 60000000, 1),
+       (3, "The Big 5", 4000000000, 2),
+       (4, "Admiral", 1000000000, 3),
+       (5, "Vice-Admiral", 500000000, 3),
+       (6, "Current", 650000000, 4)
+       (7, "Former", 450000000, 4),
+       (8, "Pirate", 300000000, 5),
+       (9, "Bystander", 500, 6)
