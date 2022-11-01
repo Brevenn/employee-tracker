2d3554a352e7df120201e3232170154cc7642a1c
Brevenn <Brevenjh@gmail.com>
2022-11-01 09:44:10 -0500

    began working on function to add employees

--- employeestats.js
+++ employeestats.js
@@ -22,149 +22,177 @@ connection.connect(function (err) {
 
 // Create function to begin prompt for user actions in the command-line.
 function start() {
-    inquirer
-        .prompt([
-            {
-                type: "list",
-                name: "beginningOptions",
-                message: "What would you like to do?",
-                choices: [
-                    "Add New Department Type",
-                    "Add New Department Role",
-                    "Add New Department",
-                    "View all Department Types",
-                    "View all Department Roles",
-                    "View all Departments",
-                    "Change the Role of a Department",
-                    "Exit Simulation"
-                ]
-            }
-        ])
-        .then(answers => {
-            switch(answers.beginningOptions) {
-                case "Add New Department Type":
-                    addCharType();
-                    break;
-                case "Add New Department Role":
-                    addCharRole();
-                    break;
-                case "Add New Department":
-                    addNewChar();
-                    break;
-                case "View All Department Types":
-                    viewCharTypes();
-                    break;
-                case "View All Department Roles":
-                    viewCharRoles();
-                    break;
-                case "View All Departments":
-                    viewAllChar();
-                    break;
-                case "Change the Role of a Department":
-                    changeRole();
-                    break;
-                default:
-                    console.log("Until Next Time!");
-                    process.exit();
-            };
-        });
-};
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "beginningOptions",
+        message: "What would you like to do?",
+        choices: [
+          "Add New Department Type",
+          "Add New Department Role",
+          "Add New Department",
+          "View all Department Types",
+          "View all Department Roles",
+          "View all Departments",
+          "Change the Role of a Department",
+          "Exit Simulation",
+        ],
+      },
+    ])
+    .then((answers) => {
+      switch (answers.beginningOptions) {
+        case "Add New Department Type":
+          addCharType();
+          break;
+        case "Add New Department Role":
+          addCharRole();
+          break;
+        case "Add New Department":
+          addNewChar();
+          break;
+        case "View All Department Types":
+          viewCharTypes();
+          break;
+        case "View All Department Roles":
+          viewCharRoles();
+          break;
+        case "View All Departments":
+          viewAllChar();
+          break;
+        case "Change the Role of a Department":
+          changeRole();
+          break;
+        default:
+          console.log("Until Next Time!");
+          process.exit();
+      }
+    });
+}
 
 // Create a function to add character types to the type seed.
 function addCharType() {
-    inquirer.prompt([
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "department type",
+        message: "Add Department Type: ",
+      },
+    ])
+    .then((answers) => {
+      connection.query(
+        "INSERT INTO department SET?",
         {
-            type: "input",
-            name: "department type",
-            message: "Add Department Type: "
+          department_name: answers.department,
+        },
+        function (err) {
+          if (err) throw err;
+          console.log("New Department added to database");
+          start();
         }
-    ]).then(answers => {
-        connection.query(
-            "INSERT INTO department SET?",
-            {
-                department_name: answers.department,
-            },
-            function(err) {
-                if (err) throw err;
-                console.log("New Department added to database");
-                start();
-            }
-        );
+      );
     });
-};
+}
 
 // Create a function to add roles to the employee_role database.
 function addCharRole() {
-    let departmentName = []
+  let departmentName = [];
 
-    promisemysql.createConnection(connectPack)
+  promisemysql
+    .createConnection(connectPack)
     .then((dbconnection) => {
-        return Promise.all([
-            dbconnection.query("SELECT * FROM department"),
-        ]);
-
+      return Promise.all([dbconnection.query("SELECT * FROM department")]);
     })
     .then(([department]) => {
-        for (var i = 0; i < department.length; i++) {
-            departmentName.push(department[i].department_name);
-        }
-        return Promise.all([department]);
+      for (var i = 0; i < department.length; i++) {
+        departmentName.push(department[i].department_name);
+      }
+      return Promise.all([department]);
     })
     .then(([department]) => {
-        inquirer.prompt ([
-            {
-                type: "input",
-                name: "role",
-                message: "Add Employee Role: ",
-                validate: function(input){
-                    if (input === ""){
-                        console.log("Employee Role Required to Proceed");
-                        return false;
-                    }
-                    else {
-                        return true;
-                    }
-                }
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "role",
+            message: "Add Employee Role: ",
+            validate: function (input) {
+              if (input === "") {
+                console.log("Employee Role Required to Proceed");
+                return false;
+              } else {
+                return true;
+              }
             },
-            {
-                type: "input",
-                name: "berries",
-                message: "Employee Role Berries: ",
-                validate: function(value) {
-                    if (isNaN(value) === false){
-                        return true;
-                    }
-                    return false;
-                }
+          },
+          {
+            type: "input",
+            name: "berries",
+            message: "Employee Role Berries: ",
+            validate: function (value) {
+              if (isNaN(value) === false) {
+                return true;
+              }
+              return false;
             },
+          },
+          {
+            type: "list",
+            name: "department",
+            message: "Department for this Role: ",
+            choices: departmentName,
+          },
+        ])
+        .then((answers) => {
+          let departmentID;
+
+          for (var i = 0; i < answers.length; i++) {
+            if (answers.department === department[i].department_name) {
+              departmentID = department[i].id;
+            }
+          }
+
+          connection.query(
+            "INSERT INTO employee_role SET ?",
             {
-                type: "list",
-                name: "department",
-                message: "Department for this Role: ",
-                choices: departmentName
+              title: answers.role,
+              berries: answers.berries,
+              department_id: departmentID,
+            },
+            function (err) {
+              if (err) throw err;
+              console.log("Employee Role added successfully!");
+              start();
             }
-        ]).then(answers => {
-            let departmentID;
+          );
+        });
+    });
+}
 
-            for (var i = 0; i < answers.length; i++) {
-                if (answers.department === department[i].department_name) {
-                    departmentID = department[i].id;
-                }
-            }
+// Create a function to add characters to the character database.
+function addNewChar() {
+  let employeeRole = [];
+  let employees = [];
 
-            connection.query(
-                "INSERT INTO employee_role SET ?",
-                {
-                    title: answers.role,
-                    berries: answers.berries,
-                    department_id: departmentID
-                },
-                function (err) {
-                    if (err) throw err;
-                    console.log("Employee Role added successfully!");
-                    start();
-                }
-            );
-        })
-    })
-};
+  promisemysql.createConnection(connectPack).then((dbconnection) => {
+    return Promise.all([
+      dbconnection.query("SELECT * FROM employee_role"),
+
+      dbconnection.query(
+        "SELECT employee.id, concat(employee.first_name, ' ' , employee.last_name) AS fullName FROM employee ORDER BY fullName ASC"
+      )
+    ]);
+  })
+  .then(([role, name]) => {
+    for (var i = 0; i < role.length; i++) {
+        employeeRole.push(role[i].title);
+    }
+
+    for (var i = 0; i < name.length; i++) {
+        employees.push(name[i].fullName)
+    }
+
+    return Promise.all(([role,name]));
+  })
+}
