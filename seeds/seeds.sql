4d0cf6ec0f99eca43a0dc0ba82feabef212f5c46
Brevenn <Brevenjh@gmail.com>
2022-10-31 19:11:25 -0500

    added department roles to characters

---
+++ seeds/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO department (id, department_name)
+VALUES (1, "Pirate Captain"),
+       (2, "Yonkou"),
+       (3, "Navy Admiral"),
+       (4, "Warlord of the Sea"), 
+       (5, "Worst Generation Pirate"), 
+       (6, "Crew Member");
